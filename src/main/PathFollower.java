ba13ab26b34d77937aac31ee7040cc5265430b2b
xiaoxiae <tomas.slama.131@gmail.com>
2019-04-03 20:42:33 +0200

    overhaul Java classes
    Since some people are actually looking at this repository, it might be a good idea to clean it up a bit.
    
    This change makes the variable names more clear, changes the style of the comments, and, most importantly, changes the way the lookahead point is calculated to be more precise, since it doesn't use trigonometric functions anymore.

--- src/main/PathFollower.java
+++ src/main/PathFollower.java
@@ -1,40 +1,40 @@
 package main;
 
 class PathFollower {
-    // Coordinates of the follower.
+    // follower coordinates
     private float[] position;
 
+    // follower speed
     private float speed;
 
     PathFollower(float x, float y, float speed) {
         this.position = new float[]{x, y};
-
         this.speed = speed;
     }
 
     /**
-     * Moves the follower towards a point at the follower's speed.
+     * Moves the follower towards a point by the follower's speed.
      *
-     * @param xToMoveTowards The x value of the coordinate towards which to move.
-     * @param yToMoveTowards The y value of the coordinate towards which to move.
+     * @param x The x value of the coordinate towards which to move.
+     * @param y The y value of the coordinate towards which to move.
      */
-    void moveFollowerTowardsPoint(float xToMoveTowards, float yToMoveTowards) {
-        // Offset the point
-        float offsetX = xToMoveTowards - position[0];
-        float offsetY = yToMoveTowards - position[1];
+    void moveFollowerTowardsPoint(float x, float y) {
+        // move the point to origin (the follower's coordinates)
+        float offsetX = x - position[0];
+        float offsetY = y - position[1];
 
-        // Normalize the vector
-        float lengthToPoint = (float) Math.sqrt(offsetX * offsetX + offsetY * offsetY);
-        float normalizedPointX = offsetX / lengthToPoint;
-        float normalizedPointY = offsetY / lengthToPoint;
+        // normalize the vector
+        float distanceToPoint = (float) Math.sqrt(offsetX * offsetX + offsetY * offsetY);
+        float normalizedX = offsetX / distanceToPoint;
+        float normalizedY = offsetY / distanceToPoint;
 
-        // Move towards the point at a certain speed
-        position[0] += normalizedPointX * speed;
-        position[1] += normalizedPointY * speed;
+        // move towards the point at a certain speed
+        position[0] += normalizedX * speed;
+        position[1] += normalizedY * speed;
     }
 
     /**
-     * Returns the current coordinates of the follower.
+     * Returns the coordinates of the follower.
      *
      * @return A float[2], with arr[0] being the x value and arr[1] being the y value.
      */
--- src/main/PurePursuit.java
+++ src/main/PurePursuit.java
@@ -1,32 +1,33 @@
 package main;
 
-import processing.core.*;
+import processing.core.PApplet;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PurePursuit extends PApplet {
-    // List of the points
+    // list of the points of the path
     private List<float[]> path;
 
-    // List of a path drawn by the follower
+    // list of the points of the path drawn by the follower
     private List<float[]> followerPath;
 
-    // A PathFollower object and its variables
+    // a PathFollower object and its variables
     private PathFollower follower;
     private float followerSpeed = 2.5f;
     private float followerStopDistance = 2;
 
-    // The increment and decrement in lookahead from keyboard input
+    // the value by which the stop distance changes
     private float lookaheadDistanceDelta = 2.5f;
 
-    //Size of the ellipses
+    // size of the points
     private float pointSize = 4;
 
-    // The distance to look ahead (in pixels)
+    // the lookahead distance
     private float lookaheadDistance = 45;
 
-    // The colour of the pursued point
+    // the color of the pursued point
     private int[] pursuedCircleColor = new int[]{255, 0, 0};
 
     static public void main(String[] passedArgs) {
@@ -40,13 +41,11 @@ public class PurePursuit extends PApplet {
 
     @Override
     public void setup() {
-        colorMode(RGB);
-
         reset();
     }
 
     /**
-     * Reset the simulation.
+     * Resets the simulation by setting the follower and all of the paths to equal null.
      */
     private void reset() {
         path = new ArrayList<>();
@@ -59,96 +58,85 @@ public class PurePursuit extends PApplet {
     public void draw() {
         background(255);
 
-        // If there are any points on the follower's path
+        // draw the follower's path
         if (followerPath != null) {
-            // Iterate through all follower path points and draw them
-            for (int i = 0; i < followerPath.size(); i+= 4) {
+            // skip some of the lines (increment by more than 1) to make the line seem dotted
+            for (int i = 0; i < followerPath.size() - 1; i += 4) {
 
-                // Coordinates of the point
-                float[] pointCoords = followerPath.get(i);
+                float[] p1 = followerPath.get(i);
+                float[] p2 = followerPath.get(i + 1);
 
-                // If it isn't the first point, connect this point to its predecessor
-                if (i > 0) {
-                    // Coords of the previous point
-                    float[] prevPointCoords = followerPath.get(i - 1);
-
-                    // Draw the line
-                    stroke(120);
-                    line(pointCoords[0], pointCoords[1], prevPointCoords[0], prevPointCoords[1]);
-                }
+                stroke(120);
+                line(p1[0], p1[1], p2[0], p2[1]);
             }
         }
 
-        // Iterate through all path points and draw them
+        // draw the path
         for (int i = 0; i < path.size(); i++) {
-            // Coordinates of the point
-            float[] pointCoords = path.get(i);
+            float[] point = path.get(i);
 
-            // Create an eclipse as the point
+            // create an ellipse to symbolize a point
             stroke(0);
             fill(0);
-            ellipse(pointCoords[0], pointCoords[1], pointSize, pointSize);
+            ellipse(point[0], point[1], pointSize, pointSize);
 
-            // If it isn't the first point, connect this point to its predecessor
+            // if it isn't the first point, connect it to its predecessor
             if (i > 0) {
-                // Coords of the previous point
-                float[] prevPointCoords = path.get(i - 1);
+                float[] previousPoint = path.get(i - 1);
 
-                // Draw the line
-                line(pointCoords[0], pointCoords[1], prevPointCoords[0], prevPointCoords[1]);
+                stroke(0);
+                line(point[0], point[1], previousPoint[0], previousPoint[1]);
             }
         }
 
-        // If mouse was pressed, get the lookahead point
+        // if mouse was pressed, attempt to draw the lookahead point from the mouse coords
         if (mousePressed && mouseButton == LEFT) {
             int x = mouseX;
             int y = mouseY;
 
-            // Get the lookahead point from the mouse coordinates
             float[] lookaheadPoint = getLookaheadPoint(x, y, lookaheadDistance);
 
-            // If the function returned a valid point, draw it
-            if (lookaheadPoint != null) drawLookaheadPoint(x, y, lookaheadPoint[0], lookaheadPoint[1]);
+            // if the point is valid, draw it
+            if (lookaheadPoint != null) {
+                drawLookaheadPoint(x, y, lookaheadPoint[0], lookaheadPoint[1]);
+            }
         }
 
-        // Draw and potentially moves the PathFollower
+        // draw and potentially move the PathFollower
         if (follower != null) {
-            // Positions of the follower and its lookahead point
-            float[] followerPosition = follower.getFollowerPosition();
-            float[] lookaheadCoordinates = getLookaheadPoint(followerPosition[0], followerPosition[1], lookaheadDistance);
+            float[] position = follower.getFollowerPosition();
+            float[] lookahead = getLookaheadPoint(position[0], position[1], lookaheadDistance);
 
-            // Draw the follower
+            // draw the follower
             fill(0);
-            ellipse(followerPosition[0], followerPosition[1], pointSize, pointSize);
+            ellipse(position[0], position[1], pointSize, pointSize);
 
-            // If lookahead coordinates for the follower exist
-            if (lookaheadCoordinates != null) {
-                // To calculate the distance between the lookahead point and the follower
-                double offsetLookaheadX = lookaheadCoordinates[0] - followerPosition[0];
-                double offsetLookaheadY = lookaheadCoordinates[1] - followerPosition[1];
+            // if lookahead exists
+            if (lookahead != null) {
+                // draw the lookahead point
+                drawLookaheadPoint(position[0], position[1], lookahead[0], lookahead[1]);
 
-                // Draw the lookahead point
-                drawLookaheadPoint(followerPosition[0], followerPosition[1], lookaheadCoordinates[0], lookaheadCoordinates[1]);
+                // calculate the distance to the lookahead point
+                double deltaX = lookahead[0] - position[0];
+                double deltaY = lookahead[1] - position[1];
+                float distance = 2 * (float) Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
 
-                // The distance from follower position to lookahead position
-                float distance = 2 * (float)Math.sqrt(Math.pow(lookaheadCoordinates[0] - followerPosition[0], 2) + Math.pow(lookaheadCoordinates[1] - followerPosition[1], 2));
-
-                // Circle around the follower
+                // draw the circle around the follower
                 noFill();
-                ellipse(followerPosition[0], followerPosition[1], distance, distance);
+                ellipse(position[0], position[1], distance, distance);
 
-                // If the follower reached the destination, delete the follower
-                if (Math.sqrt(offsetLookaheadX * offsetLookaheadX + offsetLookaheadY * offsetLookaheadY) < followerStopDistance) {
+                // if the follower reached the destination, delete the follower
+                if (distance < followerStopDistance) {
                     follower = null;
                 } else {
-                    // Move the follower upon pressing 'f'
+                    // move the follower upon pressing 'f'
                     if (keyPressed && key == 'f') {
-                        // We need to create a new coordinate pair, because the position of the pathFollower changes
-                        float[] tempFollowerPosition = follower.getFollowerPosition();
+                        // add the follower's current position to its path
+                        float[] followerPosition = follower.getFollowerPosition();
+                        followerPath.add(new float[]{followerPosition[0], followerPosition[1]});
 
-                        // Add new point to the follower's path and move the follower
-                        followerPath.add(new float[]{tempFollowerPosition[0], tempFollowerPosition[1]});
-                        follower.moveFollowerTowardsPoint(lookaheadCoordinates[0], lookaheadCoordinates[1]);
+                        // move it
+                        follower.moveFollowerTowardsPoint(lookahead[0], lookahead[1]);
                     }
                 }
             }
@@ -156,64 +144,94 @@ public class PurePursuit extends PApplet {
     }
 
     /**
-     * Generate a lookahead point on the path.
+     * Returns the sign of the input number n. Note that the function returns 1 for n = 0 to satisfy the requirements
+     * set forth by the line-circle intersection formula.
+     *
+     * @param n The number to return the sign of.
+     * @return A float value of the sign of the number (-1.0f for n < 0, else 1.0f).
+     */
+    private float signum(float n) {
+        if (n == 0) return 1;
+        else return Math.signum(n);
+    }
+
+    /**
+     * Generate the furthest lookahead point on the path that is distance r from the point (x, y).
      *
      * @param x The x of the origin.
      * @param y The y of the origin.
+     * @param r The lookahead distance.
      * @return A float[] coordinate pair if the lookahead point exists, or null.
+     * @see <a href="http://mathworld.wolfram.com/Circle-LineIntersection.html">Circle-Line Intersection</a>
      */
-    private float[] getLookaheadPoint(float x, float y, float lookaheadDistance) {
-        // The point that will be selected to be pursued from the line segments
-        float[] lookaheadPoint = new float[2];
+    private float[] getLookaheadPoint(float x, float y, float r) {
+        float[] lookahead = null;
 
-        // Iterate through all the points
+        // iterate through all pairs of points
         for (int i = 0; i < path.size() - 1; i++) {
-            // The path segment points
-            float[] lineStartPoints = path.get(i);
-            float[] lineEndPoints = path.get(i + 1);
+            // form a segment from each two adjacent points
+            float[] segmentStart = path.get(i);
+            float[] segmentEnd = path.get(i + 1);
 
-            // Translated path segment and the mouse coordinates
-            float[] translatedCoords = new float[]{lineEndPoints[0] - lineStartPoints[0], lineEndPoints[1] - lineStartPoints[1]};
-            float[] translatedMouseCoords = new float[]{x - lineStartPoints[0], y - lineStartPoints[1]};
+            // translate the segment to the origin
+            float[] p1 = new float[]{segmentStart[0] - x, segmentStart[1] - y};
+            float[] p2 = new float[]{segmentEnd[0] - x, segmentEnd[1] - y};
 
-            // The angle to turn all coordinates by
-            float angle = -(float) Math.atan2(translatedCoords[1], translatedCoords[0]);
+            // calculate an intersection of a segment and a circle with radius r (lookahead) and origin (0, 0)
+            float dx = p2[0] - p1[0];
+            float dy = p2[1] - p1[1];
+            float d = (float) Math.sqrt(dx * dx + dy * dy);
+            float D = p1[0] * p2[1] - p2[0] * p1[1];
 
-            // Translated and rotated path segment and the mouse coordinates
-            float[] turnedCoords = new float[]{translatedCoords[0] * (float) Math.cos(angle) - translatedCoords[1] * (float) Math.sin(angle), 0};
-            float[] turnedMouseCoords = new float[]{translatedMouseCoords[0] * (float) Math.cos(angle) - translatedMouseCoords[1] * (float) Math.sin(angle), translatedMouseCoords[1] * (float) Math.cos(angle) + translatedMouseCoords[0] * (float) Math.sin(angle)};
+            // if the discriminant is zero or the points are equal, there is no intersection
+            float discriminant = r * r * d * d - D * D;
+            if (discriminant < 0 || Arrays.equals(p1, p2)) continue;
 
-            // The distance from the mouse's x coordinate above the line segment to the possible point on the line segment
-            // Take note that if the segment is too far, the result of this operation wil be NaN (we can't create a  right triangle if a side is longer than a hypotenuse)
-            float intersectingPointX = turnedMouseCoords[0] + (float) Math.sqrt(lookaheadDistance * lookaheadDistance - turnedMouseCoords[1] * turnedMouseCoords[1]);
+            // the x components of the intersecting points
+            float x1 = (float) (D * dy + signum(dy) * dx * Math.sqrt(discriminant)) / (d * d);
+            float x2 = (float) (D * dy - signum(dy) * dx * Math.sqrt(discriminant)) / (d * d);
 
-            // If the point lays on the translated and rotated segment
-            if ((intersectingPointX > 0) && (intersectingPointX < turnedCoords[0])) {
-                lookaheadPoint[0] = intersectingPointX * cos(-angle) + lineStartPoints[0];
-                lookaheadPoint[1] = intersectingPointX * sin(-angle) + lineStartPoints[1];
+            // the y components of the intersecting points
+            float y1 = (float) (-D * dx + Math.abs(dy) * Math.sqrt(discriminant)) / (d * d);
+            float y2 = (float) (-D * dx - Math.abs(dy) * Math.sqrt(discriminant)) / (d * d);
+
+            // whether each of the intersections are within the segment (and not the entire line)
+            boolean validIntersection1 = Math.min(p1[0], p2[0]) < x1 && x1 < Math.max(p1[0], p2[0])
+                    || Math.min(p1[1], p2[1]) < y1 && y1 < Math.max(p1[1], p2[1]);
+            boolean validIntersection2 = Math.min(p1[0], p2[0]) < x2 && x2 < Math.max(p1[0], p2[0])
+                    || Math.min(p1[1], p2[1]) < y2 && y2 < Math.max(p1[1], p2[1]);
+
+            // remove the old lookahead if either of the points will be selected as the lookahead
+            if (validIntersection1 || validIntersection2) lookahead = null;
+
+            // select the first one if it's valid
+            if (validIntersection1) {
+                lookahead = new float[]{x1 + x, y1 + y};
+            }
+
+            // select the second one if it's valid and either lookahead is none,
+            // or it's closer to the end of the segment than the first intersection
+            if (validIntersection2) {
+                if (lookahead == null || Math.abs(x1 - p2[0]) > Math.abs(x2 - p2[0]) || Math.abs(y1 - p2[1]) > Math.abs(y2 - p2[1])) {
+                    lookahead = new float[]{x2 + x, y2 + y};
+                }
             }
         }
 
-        // Do we even have any points to draw? If we do, attempt to do so.
+        // special case for the very last point on the path
         if (path.size() > 0) {
-            // If the mouse is close enough to the end, simply select that as the pursuit target
-            float[] endPointCoordinates = path.get(path.size() - 1);
+            float[] lastPoint = path.get(path.size() - 1);
 
-            float endX = endPointCoordinates[0];
-            float endY = endPointCoordinates[1];
+            float endX = lastPoint[0];
+            float endY = lastPoint[1];
 
-            if (Math.sqrt((endX - x) * (endX - x) + (endY - y) * (endY - y)) <= lookaheadDistance) {
-                lookaheadPoint[0] = endX;
-                lookaheadPoint[1] = endY;
-            }
-
-            // If we selected any points to pursue, draw them.
-            if (lookaheadPoint[0] != 0 && lookaheadPoint[1] != 0) {
-                return new float[]{lookaheadPoint[0], lookaheadPoint[1]};
+            // if we are closer than lookahead distance to the end, set it as the lookahead
+            if (Math.sqrt((endX - x) * (endX - x) + (endY - y) * (endY - y)) <= r) {
+                return new float[]{endX, endY};
             }
         }
 
-        return null;
+        return lookahead;
     }
 
     /**
@@ -225,40 +243,51 @@ public class PurePursuit extends PApplet {
      * @param y2 The y value of the lookahead.
      */
     private void drawLookaheadPoint(float x1, float y1, float x2, float y2) {
-        // Line between object and lookahead point
+        // line between object and lookahead point
         stroke(0);
         line(x1, y1, x2, y2);
 
-        // Fill the circle with the desired color of the point to be pursued
+        // fill the circle with the desired color of the point to be pursued
         fill(pursuedCircleColor[0], pursuedCircleColor[1], pursuedCircleColor[2]);
 
-        // Lookahead point
+        // lookahead point
         ellipse(x2, y2, pointSize, pointSize);
     }
 
+    /**
+     * Is called when a key is pressed; controls most of the program controls.
+     */
     @Override
     public void keyPressed() {
-        //Reset the game
-        if (key == 'r') reset();
+        switch (key) {
+            case 'r':
+                reset();
+                break;
+            case 'n':
+                if (path.size() > 0) {
+                    float[] firstPointCoordinates = path.get(0);
 
-        // Create a new follower object at the beginning of the path
-        if (key == 'n' && path.size() > 0) {
-            float[] firstPointCoordinates = path.get(0);
-
-            followerPath = new ArrayList<>();
-            follower = new PathFollower(firstPointCoordinates[0], firstPointCoordinates[1], followerSpeed);
+                    followerPath = new ArrayList<>();
+                    follower = new PathFollower(firstPointCoordinates[0], firstPointCoordinates[1], followerSpeed);
+                }
+                break;
+            case '+':
+                lookaheadDistance += lookaheadDistanceDelta;
+                break;
+            case '-':
+                lookaheadDistance -= lookaheadDistanceDelta;
+                break;
         }
-
-        // Increase lookahead distance
-        if (key == '+') lookaheadDistance += lookaheadDistanceDelta;
-
-        // Decreased lookahead distance
-        if (key == '-') lookaheadDistance -= lookaheadDistanceDelta;
     }
 
+    /**
+     * Is called when the mouse is pressed; creates new points.
+     */
     @Override
     public void mousePressed() {
-        // Add a new path point
-        if (mouseButton == RIGHT) path.add(new float[]{mouseX, mouseY});
+        // add a new path point upon pressing the right button
+        if (mouseButton == RIGHT) {
+            path.add(new float[]{mouseX, mouseY});
+        }
     }
 }
